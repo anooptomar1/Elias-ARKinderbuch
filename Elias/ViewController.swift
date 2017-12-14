92a0608f6510380134cfca921374eef813a2066d
Janine W <Janine.weirich@kfit.de>
2017-12-14 15:40:20 +0100

    Initial Commit

---
+++ Elias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elias
+//
+//  Created by Janine Weirich on 14.12.17.
+//  Copyright © 2017 Janine Weirich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
